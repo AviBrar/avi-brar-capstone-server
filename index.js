774d5b0f9085007b52d48781c405ab0456df8931
Avi Brar <avineetsbrar@gmail.com>
2024-04-07 13:11:12 -0700

    server created

---
+++ index.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/", (_req,res)=> res.send("Hello World!"));
+
+app.listen(PORT, ()=> console.log(`Listening on port ${PORT}`));
