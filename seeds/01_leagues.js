c28c41e316b27340176aac8d615e54e912288728
Avi Brar <avineetsbrar@gmail.com>
2024-04-17 07:20:29 -0700

    changed seed data

--- seeds/01_leagues.js
+++ seeds/01_leagues.js
@@ -3,29 +3,38 @@
  * @returns { Promise<void> }
  */
 exports.seed = async function (knex) {
-    // Deletes ALL existing entries
-    await knex("leagues").del();
-    await knex("leagues").insert([
-        {
-            "id": 1,
-            "league_name": "Primera A",
-            "league_teams": 3,
-            "area": "California",
-            "country": "USA",
-            "league_administrator": "Steve Watkins",
-            "contact_phone": "9919919988",
-            "contact_email": "administrator@prim.com"
-        },
-        {
-            "id": 2,
-            "league_name": "Premier League",
-            "league_teams": 2,
-            "area": "Cascadia",
-            "country": "Canada",
-            "league_administrator": "Alex Turns",
-            "contact_phone": "6045736625",
-            "contact_email": "administrator@prem.com"
-        },
-    ]);
-  };
-  
+  // Deletes ALL existing entries
+  await knex("leagues").del();
+  await knex("leagues").insert([
+    {
+      id: 1,
+      league_name: "Primera A",
+      league_teams: 3,
+      area: "California",
+      country: "USA",
+      league_administrator: "Steve Watkins",
+      contact_phone: "9919919988",
+      contact_email: "administrator@prim.com",
+    },
+    {
+      id: 2,
+      league_name: "Premier League",
+      league_teams: 2,
+      area: "Cascadia",
+      country: "Canada",
+      league_administrator: "Alex Turns",
+      contact_phone: "6045736625",
+      contact_email: "administrator@prem.com",
+    },
+    {
+      id: 3,
+      league_name: "Vancouver South",
+      league_teams: 0,
+      area: "Vancouver",
+      country: "Canada",
+      league_administrator: "Tommy Powers",
+      contact_phone: "+1 (778) 465 2978",
+      contact_email: "administrator@vsleague.com",
+    },
+  ]);
+};
